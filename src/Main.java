738602f8ef6b9cf56fe09cb96620e99e70c97113
GBTC406001UR <GBTC406001UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-23 13:12:25 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Today is july 19, 2015. I am alive.");
+    }
+}
